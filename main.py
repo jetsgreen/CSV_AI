ead41fae44873e9b509e1792f2657d59b177b999
Felix Fernandez <jailyne02@yahoo.com>
2024-05-05 10:26:54 -0400

    added code to visualize answer from llm on streamlit interface

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain.agents.agent_types import AgentType
 from langchain_community.output_parsers.rail_parser import GuardrailsOutputParser
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
-from openai import OpenAI
+from langchain_community.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 
 
@@ -17,11 +17,13 @@ def main():
     if user_csv is not None:
         user_question = st.text_input("Ask a question about your CSV")
 
-        llm = OpenAI(temperature=0)
+        llm = ChatOpenAI(temperature=0)
         agent = create_csv_agent(llm, user_csv, verbose=True)
 
         if user_question is not None and user_question !="":
-            st.write(f"Your question was: {user_question}")
+            response = agent.run(user_question)
+            
+            st.write(response)
 
 if __name__== "__main__":
     main()
